bcfd4b87cd05d7cd6717f45db40ceb952eaa5f72
ramonbrbs <ramonflmr@gmail.com>
2018-08-10 09:56:15 -0300

    Modifica allowed_hosts

--- sistema/settings.py
+++ sistema/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9zhg64!^1fekwvn9*i09ufn0ofz*c*vh@kyfx1zcb*$cb)m&ch'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ramonbrbs.pythonanywhere.com']
 
 JET_SIDE_MENU_COMPACT = True
 
